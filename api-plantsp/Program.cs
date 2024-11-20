5f1060fba7188bd5e88cfc7cf35ec437f5d4934b
Luis Henrique <duimbispo@gmail.com>
2024-11-20 16:15:53 -0300

    Mudando credenciais do banco de dados

--- api-plantsp/Program.cs
+++ api-plantsp/Program.cs
@@ -29,11 +29,11 @@ var app = builder.Build();
 app.UseCors("AllowReactApp");
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseAuthorization();
 
